6e8b28e152a33d76d13339113bb28af47e7abf85
Pedro Lopez <plopez@poweranalytics.com>
2021-04-06 15:48:04 -0400

    Framework part 3

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,13 +1,16 @@
 import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
 import TopBar from '../pages/components/TopBar'
 
 describe('Example', () => {
 	let homepage
+	let loginpage
 	let topbar
 
 	beforeAll(async () => {
 		jest.setTimeout(15000)
 		homepage = new HomePage()
+		loginpage = new LoginPage()
 		topbar = new TopBar()
 	})
 	it('homepage should work', async () => {
@@ -17,5 +20,12 @@ describe('Example', () => {
 	it('navbar should be displayed', async () => {
 		await homepage.isNavbarDisplayed()
 		await topbar.isTopBarDisplayed()
+	})
+
+	it('try to login', async () => {
+		await loginpage.visit()
+		await loginpage.isLoginFormDisplayed()
+		await loginpage.login('username', 'password')
+		await loginpage.wait(5000)
 	})
 })
